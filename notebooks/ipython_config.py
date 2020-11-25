ea8aa03c8c6aef40c3e0e950dba04fb78baaf7d8
Gil <sgy@tpay.co.kr>
2020-11-25 13:12:50 +0900

    ipython_config.py 추가

---
+++ notebooks/ipython_config.py
@@ -0,0 +1,7 @@
+import sys
+import os
+
+FILE_PATH = os.path.abspath(os.path.dirname(__file__))
+PROJECT_BASE_PATH = os.path.abspath(os.path.join(FILE_PATH, '..'))
+
+sys.path.insert(1, PROJECT_BASE_PATH)
